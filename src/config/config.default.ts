80c809e5c4f007ee87a3cee328a5d961fe9f9110
lixin <lixin544@163.com>
2020-09-27 10:56:36 +0800

    feat add mongo

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -8,12 +8,28 @@ export default (appInfo: EggAppInfo) => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1601005102076_5946';
 
+  // 语鹊token，用于获取专栏信息
   // config.yuque = {
   //   token: '',
   // };
 
+  // mongo url
+  // config.mongoose = {
+  //   url: '',
+  //   options: {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   },
+  // };
+
   // add your config here
   config.middleware = [];
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
 
   return config;
 };
